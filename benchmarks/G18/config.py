62c8e9a38e7c848e8c56dc9ed893bae7cbfe5eae
EricRode <ericrode13@gmail.com>
2025-10-14 23:07:31 +0200

    Update config.py

--- benchmarks/G18/config.py
+++ benchmarks/G18/config.py
@@ -3,7 +3,7 @@ import string
 C1_ARG = [[[i, i+1, i+2], i+3] for i in range(1, 300000)] #Elapsed time: 38.98418426513672 seconds
 C2_ARG = [55] #Elapsed time: 30.849018096923828 seconds
 C3_ARG = [i for i in range(40)] #Elapsed time: 27.026596307754517 seconds
-C4_ARG = [i for i in range(40)] #Elapsed time: 28.851619482040405 seconds
+C4_ARG = [i for i in range(35)] #Elapsed time: 28.851619482040405 seconds
 C5_ARG = [[j for j in range(i * 10 + 1, i * 10 + 11)] for i in range(250000)] #Elapsed time: 27.230534315109253 seconds 
 C6_ARG = [str(i) for i in range(100000)] #Elapsed time: 24.837823629379272 seconds
 C7_ARG = [[[i, i+1, i+2, i+3], i+2] for i in range(1, 15000)]#Elapsed time: 38.39557123184204 seconds
